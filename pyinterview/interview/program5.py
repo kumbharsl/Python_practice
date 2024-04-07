7268c9ecd0c9b65a16a99c5925f58a199225c5cc
Sagar Kumbhar <sagarkumbhar2018@gmail.com>
2024-04-07 09:06:40 +0530

    done

--- pyinterview/interview/program5.py
+++ pyinterview/interview/program5.py
@@ -1,10 +1,23 @@
-row = int(input("Enter the row : "))
-k = 0
-for i in range(row):
-    for j in range((row-i)+1):
-        print(end = " ")
-    while k!=(i+1):
-        print("*", end = " ")
-        k+=1
-    k=0
+rows  = 5
+count = 9
+number = 5
+
+for i in range(rows):
+    num = count
+    for j in range(i):
+        print(" ", end="")
+    for k in range(i, rows):
+        print(num, end=" ")
+        num -= 1
+    count -= 1
     print()
+    
+for i in range(rows-2, -1, -1):
+    num = number
+    for j in range(i):
+        print(" ", end="")
+    for k in range(i, rows):
+        print(num, end=" ")
+        num +=1
+    print()
+    
