6bd4ced14ba98b21df91123f9e3f2083f79c3eac
Sagar Kumbhar <sagarkumbhar2018@gmail.com>
2024-06-13 20:10:19 +0530

    done

--- quize/Array/Array1/program4.py
+++ quize/Array/Array1/program4.py
@@ -1,3 +1,3 @@
 import array
-arr=array.array('f',[10,20,30])
+arr=array.array('f',[10,20,30,40,50,60,70,80,90])
 print(arr[2])
--- quize/Array/Array1/program6.py
+++ quize/Array/Array1/program6.py
@@ -1,3 +1,3 @@
 import array
-arr=array.array('i',[1,2,3,4,5,6,7,8])
+arr=array.array('i',[1,2,3,4,5,6,7,8,10])
 print(arr[2:-1])
